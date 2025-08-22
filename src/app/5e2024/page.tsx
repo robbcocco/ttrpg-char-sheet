eb1f476c973cf3eddf70e4fa5373dc0535eaf065
Roberto Cocco <robertococco@outlook.com>
2025-08-22 17:08:21 +0200

    New name who dis

--- src/app/5e2024/page.tsx
+++ src/app/5e2024/page.tsx
@@ -1,7 +1,13 @@
 import CharacterSheet from '@/components/5e2024/character-sheet';
 import { CharacterProvider } from '@/store/5e2024/character-store';
+import { Metadata } from 'next';
 
-export default function D5e2024Page() {
+export const metadata: Metadata = {
+  title: "5e Character Sheet",
+  description: "A not so very strict Character Sheet for D&D 5e (2024)",
+};
+
+export default function Page() {
   return (
     <CharacterProvider>
       <CharacterSheet />
